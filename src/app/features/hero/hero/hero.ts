8eb1e1830272e9760d8d0c54a8aeccc7128f7fee
ganciaux <ghislain.anciaux@live.fr>
2026-03-12 15:06:33 +0100

    feat(hero): add computed signals for hp percent and status, add effect for hp logging

--- src/app/features/hero/hero/hero.ts
+++ src/app/features/hero/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { createHero, Hero, HERO_MAX_HP, HERO_STAT_LIST } from '../models/hero';
 import { FormsModule } from '@angular/forms';
 
@@ -12,6 +12,24 @@ export class HeroComponent {
   readonly statList = HERO_STAT_LIST;
   readonly maxHp = HERO_MAX_HP;
   readonly hero = signal<Hero>(createHero({hp: 10}));
+  readonly hpPercent = computed(() => {
+    return (this.hero().hp / this.maxHp) * 100;
+  })
+  readonly status = computed(() => {
+    if (this.hero().hp === 0) {
+      return '💀 Dead';
+    } else if (this.hero().hp < this.maxHp * 0.3) {
+      return '💥 Critical';
+    } else if (this.hero().hp < this.maxHp * 0.7) {
+      return '🤕 Injured';
+    } else {
+      return '💪 Healthy';
+    }
+  });
+
+  constructor() {
+    effect(() => console.log('HP changed:', this.hero().hp));
+  }
 
   updateHp(amount: number) {
     this.hero.update(hero => ({
