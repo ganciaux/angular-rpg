d8f342cd0b7e4c518020bd131194a18d5609f1fc
ganciaux <ghislain.anciaux@live.fr>
2026-03-12 17:01:11 +0100

    wip(hero): add enemy model and combat logic

--- src/app/features/hero/hero/hero.ts
+++ src/app/features/hero/hero/hero.ts
@@ -1,5 +1,5 @@
 import { Component, computed, effect, signal } from '@angular/core';
-import { createHero, Hero, HERO_MAX_HP, HERO_STAT_LIST } from '../models/hero';
+import { createGoblin, createHero, Enemy, GameStatus, GAME_STATUS_LABEL, Hero, HERO_MAX_HP, HERO_STAT_LIST } from '../models/hero';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './hero.css',
 })
 export class HeroComponent {
+  readonly gameStatusLabel = GAME_STATUS_LABEL;
   readonly statList = HERO_STAT_LIST;
   readonly maxHp = HERO_MAX_HP;
+  readonly gameStatusEnum = GameStatus;
   readonly hero = signal<Hero>(createHero({hp: 10}));
+  readonly enemy = signal<Enemy>(createGoblin());
+  readonly enemiesDefeated = signal<number>(0);
   readonly hpPercent = computed(() => {
     return (this.hero().hp / this.maxHp) * 100;
   })
-  readonly status = computed(() => {
+  readonly heroStatus = computed(() => {
     if (this.hero().hp === 0) {
       return '💀 Dead';
     } else if (this.hero().hp < this.maxHp * 0.3) {
@@ -26,6 +30,12 @@ export class HeroComponent {
       return '💪 Healthy';
     }
   });
+  readonly enemyStatus = computed(() => {return '';});
+  readonly enemyHpPercent = computed(() => {
+    return (this.enemy().hp / this.enemy().hpMax!) * 100;
+  })
+
+  gameStatus = signal<GameStatus>(GameStatus.STOP);
 
   constructor() {
     effect(() => console.log('HP changed:', this.hero().hp));
@@ -38,6 +48,13 @@ export class HeroComponent {
     }));
   }
 
+  enemyTakeDamage(amount: number) {
+    this.enemy.update(enemy => ({
+      ...enemy,
+      hp: Math.max(enemy.hp - amount, 0)
+    }));
+  }
+
   updateName(event: Event) {
     const input = event.target as HTMLInputElement;
     this.hero.update(hero => ({
@@ -46,4 +63,48 @@ export class HeroComponent {
     }));
   }
 
-}
+  startGame() {
+    this.enemiesDefeated.set(0);
+    this.hero.set(createHero());
+    this.enemy.set(createGoblin());
+    this.gameStatus.set(GameStatus.PLAYING);
+  }
+
+  stopGame() {
+    this.gameStatus.set(GameStatus.STOP);
+  }
+
+  restartGame() {
+    this.enemiesDefeated.set(0);
+    this.hero.set(createHero());
+    this.enemy.set(createGoblin());
+    this.gameStatus.set(GameStatus.PLAYING);
+  }
+
+  nextLevel() {
+    let currentHero = this.hero();
+    this.enemiesDefeated.update(count => count + 1);
+    this.enemy.set(createGoblin());
+    this.updateHp(currentHero.hp + (this.maxHp - currentHero.hp) / 2);
+  }
+
+  attack() {
+    const currentHero = this.hero();
+    const currentEnemy = this.enemy();
+    let damage = Math.floor(Math.random() * (currentHero.attack + 1));
+    let isCrit = Math.random() < 0.1;
+    let finalDamage = isCrit ? damage * 2 : damage;
+
+    this.enemyTakeDamage(finalDamage);
+
+    if (this.enemy().hp>0){
+      damage = Math.floor(Math.random() * (currentEnemy.attack + 1));
+      isCrit = Math.random() < 0.1;
+      finalDamage = isCrit ? damage * 2 : damage;
+      this.updateHp(-finalDamage);
+    } 
+    else{
+      this.gameStatus.set(GameStatus.VICTORY);
+    }
+  }
+} 
--- src/app/features/hero/models/hero.ts
+++ src/app/features/hero/models/hero.ts
@@ -1,9 +1,32 @@
+export enum GameStatus {
+  STOP,
+  PLAYING,
+  VICTORY,
+  GAME_OVER,
+}
+
+export const GAME_STATUS_LABEL: Record<GameStatus, string> = {
+  [GameStatus.STOP]: 'Stop',
+  [GameStatus.PLAYING]: 'Playing',
+  [GameStatus.VICTORY]: 'Victory',
+  [GameStatus.GAME_OVER]: 'Game Over',
+};
+
 export interface Hero {
   id: string;
   name: string;
   hp: number;
   level: number;
+  attack: number;
   stats: HeroStats;
+}
+
+export interface Enemy {
+  id: string;
+  name: string;
+  hp: number;
+  hpMax?: number;
+  attack: number;
 }
 
 export enum HeroStatLabel {
@@ -17,6 +40,7 @@ export type HeroStats = Record<HeroStatLabel, number>;
 export const HERO_STAT_LIST = Object.values(HeroStatLabel) as HeroStatLabel[];
 
 export const HERO_INITIAL_HP = 100;
+export const HERO_INITIAL_ATTACK = 10;
 export const HERO_INITIAL_LEVEL = 1;
 export const HERO_INITIAL_NAME = 'Hero';
 export const HERO_MAX_HP = 100;
@@ -33,7 +57,28 @@ export function createHero(partial?: Partial<Hero>): Hero {
     name: HERO_INITIAL_NAME,
     hp: HERO_INITIAL_HP,
     level: HERO_INITIAL_LEVEL,
-    stats: {...HERO_INITIAL_STATS},
+    attack: HERO_INITIAL_ATTACK,
+    stats: { ...HERO_INITIAL_STATS },
     ...partial,
   };
+}
+
+export function createEnemy(partial?: Partial<Enemy>): Enemy {
+  return {
+    id: crypto.randomUUID(),
+    name: 'Enemy',
+    hp: HERO_INITIAL_HP,
+    hpMax: HERO_INITIAL_HP,
+    attack: HERO_INITIAL_ATTACK,
+    ...partial,
+  };
+}
+
+export function createGoblin(): Enemy {
+  return createEnemy({
+    name: 'Goblin',
+    hp: 30,
+    hpMax: 30,
+    attack: 5,
+  });
 }
