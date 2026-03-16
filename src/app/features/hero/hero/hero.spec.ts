0843a3e3e8179e4b26e0e54c3fa8dabd7f9303b1
ganciaux <ghislain.anciaux@live.fr>
2026-03-16 10:32:25 +0100

    test(hero): fix hero component tests to use HeroService

--- src/app/features/hero/hero/hero.spec.ts
+++ src/app/features/hero/hero/hero.spec.ts
@@ -1,18 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { HeroComponent } from './hero';
 import {
-  HeroStatLabel,
   HERO_INITIAL_HP,
   HERO_INITIAL_LEVEL,
   HERO_INITIAL_NAME,
   HERO_INITIAL_STATS,
+  HERO_MAX_HP,
+  HERO_INITIAL_ATTACK,
 } from '../models/hero';
+import { HeroService } from '../services/hero';
 
 describe('HeroComponent', () => {
+  let heroService: HeroService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeroComponent],
+      providers: [HeroService],
     }).compileComponents();
+    heroService = TestBed.inject(HeroService);
   });
 
   it('should create the component', () => {
@@ -21,22 +26,22 @@ describe('HeroComponent', () => {
   });
 
   it('should have correct initial hero stats', () => {
-    const fixture = TestBed.createComponent(HeroComponent);
-    const component = fixture.componentInstance;
-    expect(component.hero().name).toEqual(HERO_INITIAL_NAME);
-    expect(component.hero().hp).toEqual(HERO_INITIAL_HP);
-    expect(component.hero().level).toEqual(HERO_INITIAL_LEVEL);
-    expect(component.hero().stats).toEqual({ ...HERO_INITIAL_STATS });
+    
+    expect(heroService.hero().name).toEqual(HERO_INITIAL_NAME);
+    expect(heroService.hero().hp).toEqual(HERO_INITIAL_HP);
+    expect(heroService.hero().level).toEqual(HERO_INITIAL_LEVEL);
+    expect(heroService.hero().hpMax).toEqual(HERO_MAX_HP);
+    expect(heroService.hero().attack).toEqual(HERO_INITIAL_ATTACK);
+    expect(heroService.hero().stats).toEqual({ ...HERO_INITIAL_STATS });
   });
 
   it('should display dead message when hp is 0', () => {
     const fixture = TestBed.createComponent(HeroComponent);
-    const component = fixture.componentInstance;
 
-    component.hero.set({ ...component.hero(), hp: 0 });
+    heroService.hero.set({ ...heroService.hero(), hp: 0 });
     fixture.detectChanges();
 
     const el = fixture.nativeElement as HTMLElement;
-    expect(el.textContent).toContain('💀 Héros mort');
+    expect(el.textContent).toContain('💀 Dead');
   });
 });
